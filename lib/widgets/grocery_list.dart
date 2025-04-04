38fdb816b5caa88534bcda360ab3c217a5dae99a
Satyam Prajapati <satyam7355@gmail.com>
2025-04-04 23:45:08 +0530

    get data from backend on grocery list work did

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:shopping_list/data/categories.dart';
 
 import 'package:shopping_list/models/grocery_item.dart';
 import 'package:shopping_list/widgets/new_item.dart';
@@ -12,7 +15,7 @@ class GroceryList extends StatefulWidget {
 }
 
 class _GroceryListState extends State<GroceryList> {
-  final List<GroceryItem> _groceryItems = [];
+  List<GroceryItem> _groceryItems = [];
 
   @override
   void initState() {
@@ -27,7 +30,28 @@ class _GroceryListState extends State<GroceryList> {
     );
 
     final respone = await http.get(url);
-    print(respone.body);
+    final Map<String, dynamic> listData = json.decode(respone.body);
+    final List<GroceryItem> _loadedItems = [];
+    for (final item in listData.entries) {
+      final category =
+          categories.entries
+              .firstWhere(
+                (catItem) => catItem.value.title == item.value['category'],
+              )
+              .value;
+      _loadedItems.add(
+        GroceryItem(
+          id: item.key,
+          name: item.value['name'],
+          quantity: item.value['quantity'],
+          category: category,
+        ),
+      );
+    }
+
+   setState(() {
+      _groceryItems = _loadedItems;
+    });
   }
 
   void _addItem() async {
