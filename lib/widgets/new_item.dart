9f722c438703784d854604852f487711ae6b1acd
Satyam Prajapati <satyam7355pra@gmail.com>
2025-04-04 16:40:29 +0530

    using http date stored in firebase database

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/category.dart';
 import 'package:shopping_list/models/grocery_item.dart';
+import 'package:http/http.dart' as http;
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -19,16 +22,35 @@ class _NewItemState extends State<NewItem> {
   var _selectedCategory = categories[Categories.vegetables]!;
 
   void _saveItem() {
-    if (_formKey.currentState!.validate()) {
+    // if (_formKey.currentState!.validate()) {
+    //   _formKey.currentState!.save();
+    //   Navigator.of(context).pop(
+    //     GroceryItem(
+    //       id: DateTime.now().toString(),
+    //       name: _enteredName,
+    //       quantity: _enteredQuantity,
+    //       category: _selectedCategory,
+    //     ),
+    //   );
+    // }
+      if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
-      Navigator.of(context).pop(
-        GroceryItem(
-          id: DateTime.now().toString(),
-          name: _enteredName,
-          quantity: _enteredQuantity,
-          category: _selectedCategory,
+      final url = Uri.https(
+          'shoppinglistflutter-ad46c-default-rtdb.firebaseio.com', 'shopping-list.json');
+      http.post(
+        url,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: json.encode(
+          {
+            'name': _enteredName,
+            'quantity': _enteredQuantity,
+            'category': _selectedCategory.title,
+          },
         ),
       );
+      // Navigator.of(context).pop();
     }
   }
 
